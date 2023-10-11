55d06ec719c3011ed3c776651ff7ed93d7686155
KwangHo <skh990427@naver.com>
2023-10-11 15:17:15 +0900

    repository 생성

---
+++ src/main/java/company/cslee/board/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package company.cslee.board.repository;
+
+import company.cslee.board.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
