30acd3a631da2d50d207b5008f9324e1eb6b744f
KwangHo <skh990427@naver.com>
2023-10-12 12:40:04 +0900

    csrf disable 메소드 주석 설명 추가

--- src/main/java/company/cslee/board/config/SecurityConfig.java
+++ src/main/java/company/cslee/board/config/SecurityConfig.java
@@ -19,7 +19,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
         http
                 .csrf().disable();
-
+        /**
+         * csrf 메소드와 disable 메소드는 CSRF(크로스 사이트 요청 위조)라는 인증된 사용자를
+         * 이용해 서버에 위험을 끼치는 요청들을 보내는 공격을 방어하기 위해
+         * POST 요청마다 token이 필요한 과정을 생략하겠음을 의미
+         */
         return http.build();
     }
 }
