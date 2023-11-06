3a3b2247f095b83d0e4892c6d767a4b1d30b1baa
KwangHo <skh990427@naver.com>
2023-11-06 16:11:32 +0900

    feat: 로그인 시 쿠키에 user_id 값 전달

--- src/main/java/company/cslee/board/controller/UserController.java
+++ src/main/java/company/cslee/board/controller/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 @RestController
@@ -24,9 +26,18 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public Optional<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
+    public Optional<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse httpServletResponse) {
         System.out.println("loginRequestDto = " + loginRequestDto.getEmail());
-        return userService.login(loginRequestDto);
+        Optional<LoginResponseDto> loginUser = userService.login(loginRequestDto);
+
+        if (loginUser.isPresent()) {
+            Cookie cookie = new Cookie("user_id", String.valueOf(loginUser.get().getId()));
+            httpServletResponse.addCookie(cookie);
+
+            return loginUser;
+        } else {
+            return Optional.empty();
+        }
     }
 
 }
