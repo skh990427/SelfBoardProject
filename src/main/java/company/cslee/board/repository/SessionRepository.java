8079d98ff84dc936507cf12eca360540fccbbf12
KwangHo <skh990427@naver.com>
2023-11-06 11:33:05 +0900

    refactor: 쿠키 사용할 예정이라 Session Entity, Repository 제거

--- src/main/java/company/cslee/board/repository/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package company.cslee.board.repository;
-
-import company.cslee.board.model.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SessionRepository extends JpaRepository<Session, String> {
-}
